f4a27ec89db658a745c7d4fdb697e76fca7064f4
Katy <katy.ledgard@gmail.com>
2023-06-09 11:10:15 +0100

    Added a js file

---
+++ 102/script.js
@@ -0,0 +1,19 @@
+let Name = prompt("Hello new friend, what is your name?");
+console.log(Name)
+
+const Welcome = alert("Welcome " + Name + "! I hope you enjoy my site!");
+
+
+// if (Name === "Rich"){
+//     console.log(Thanks for looking, Rich!)
+// } else if (Name === "Chris"){
+//     console.log(Great teaching, Chris!)
+// } else if (Name === "Sam"){
+//     console.log(Thanks, Sam!)
+// } else {
+//     console.log("Hello everybody else!")
+// }
+
+
+
+
